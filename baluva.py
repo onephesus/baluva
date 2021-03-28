305231f3defb7216c0c0b0e435513630f2f35224
onephesus <alexmoneill97@gmail.com>
2021-03-28 15:59:18 -0600

    Added _check_events() function

--- baluva.py
+++ baluva.py
@@ -22,10 +22,7 @@ class Baluva:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
                     
             # Redraw the screeen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
@@ -33,6 +30,13 @@ class Baluva:
             
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            
+            
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
             
 if __name__ == '__main__':
     # Make a game instance, and run the game.
